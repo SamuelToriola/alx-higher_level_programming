e431e4f36c36004bc8031156cb94edcd00ade6ac
SamuelToriola <SamuelToriola>
2022-08-03 01:41:44 +0000

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,12 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """ This is a python fuction
 that read a file """
 
-def read_file(filename=""):
-   
-    with open('filename','w',encoding="utf-8") as fw:
-            fw.write('hello world !')
-            fw.close()
-    with open('filename','r', encoding="utf-8") as fr:
-            print(fr.read())
-            fr.close()
 
-read_file()
+def read_file(filename=""):
+    """ This function takes a filename(string)
+    as an argument, create a file with the name
+    and read the content stored in it
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
